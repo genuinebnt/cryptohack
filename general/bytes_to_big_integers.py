7ea2ac004436243c3289d7883c7b1e90615c8a88
genuinebnt <genuinebnt@gmail.com>
2024-06-16 11:14:05 +0530

    solved bytes to big integars

--- general/bytes_to_big_integers.py
+++ general/bytes_to_big_integers.py
@@ -1,6 +1,16 @@
+from Crypto.Util.number import *
+
 def main():
-    int = "11515195063862318899931685488813747395775516287289682636499965282714637259206269"
-    
+    int_val = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+    flag = long_to_bytes(int_val).decode('utf-8')
+    print(flag)
+
+    ### alternate way without any library support
+    # v = str(hex(int_val))[2:]
+    # flag = ""
+    # for i in range(0, len(v), 2):
+    #     flag += chr(int(str(v[i:i+2]), 16))
+    # print(flag)
 
 if __name__ == '__main__':
     main()
